315355a3b61963628d12764e01712b587bc8747c
SonicGD <sonicgd@gmail.com>
2016-12-01 20:54:20 +0500

    game page

---
+++ src/Site/Controllers/GamesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using BioEngine.Common.DB;
+using BioEngine.Site.Base;
+using BioEngine.Site.ViewModels.Games;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioEngine.Site.Controllers
+{
+    public class GamesController : BaseController
+    {
+        public GamesController(BWContext context) : base(context)
+        {
+        }
+
+        [HttpGet("/{gameUrl:regex(^[[a-z0-9_]]+$)}.html")]
+        public IActionResult Index(string gameUrl)
+        {
+            var game = Context.Games.Include(x => x.Developer).FirstOrDefault(x => x.Url == gameUrl);
+            if (game == null)
+            {
+                return new NotFoundResult();
+            }
+
+            var lastNews =
+                Context.News.Where(x => x.GameId == game.Id && x.Pub == 1).OrderByDescending(x => x.Id).Take(5).ToList();
+            var lastArticles =
+                Context.Articles.Where(x => x.GameId == game.Id && x.Pub == 1).OrderByDescending(x => x.Id).Take(5).ToList();
+            var lastFiles =
+                Context.Files.Where(x => x.GameId == game.Id).OrderByDescending(x => x.Id).Take(5).ToList();
+            var lastPics =
+                Context.GalleryPics.Where(x => x.GameId == game.Id && x.Pub == 1).OrderByDescending(x => x.Id).Take(5).ToList();
+
+            var view = new GamePageViewModel(Settings, game, lastNews, lastArticles, lastFiles, lastPics);
+            return View(view);
+        }
+    }
+}
---
+++ src/Site/ViewModels/Games/GamePageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using BioEngine.Common.Models;
+
+namespace BioEngine.Site.ViewModels.Games
+{
+    public class GamePageViewModel : BaseViewModel
+    {
+        public GamePageViewModel(IEnumerable<Settings> settings, Game game, IEnumerable<Common.Models.News> lastNews,
+            IEnumerable<Article> lastArticles, IEnumerable<File> lastFiles, IEnumerable<GalleryPic> lastPics)
+            : base(settings)
+        {
+            Game = game;
+            LastNews = lastNews;
+            LastArticles = lastArticles;
+            LastFiles = lastFiles;
+            LastPics = lastPics;
+            Title = Game.Title;
+            ImageUrl = game.Icon;
+            Description = game.Desc;
+        }
+
+        public Game Game { get; }
+        public IEnumerable<Common.Models.News> LastNews { get; }
+        public IEnumerable<Article> LastArticles { get; }
+        public IEnumerable<File> LastFiles { get; }
+        public IEnumerable<GalleryPic> LastPics { get; }
+    }
+}

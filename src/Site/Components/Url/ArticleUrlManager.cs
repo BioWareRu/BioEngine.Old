e9ba630329c55e7ed0f8c534010168f79a3d424c
SonicGD <sonicgd@gmail.com>
2016-12-02 23:55:18 +0500

    More articles urls #5 #6

--- src/Site/Components/Url/ArticleUrlManager.cs
+++ src/Site/Components/Url/ArticleUrlManager.cs
@@ -17,15 +17,15 @@ namespace BioEngine.Site.Components.Url
         public string PublicUrl(Article article)
         {
             Poppulate(article);
+            var url = CatUrl(article.Cat) + "/" + article.Url;
             return GetUrl("Show", "Articles",
-                new {parentUrl = ParentUrl(article), catUrl = CatUrl(article), articleUrl = article.Url});
+                new {parentUrl = ParentUrl(article), url});
             /*return _urlHelper.Action<ArticlesController>(x => x.Show(ParentUrl(article), CatUrl(article), article.Url));*/
         }
 
-        private static string CatUrl(Article article)
+        private static string CatUrl(ArticleCat cat)
         {
             var urls = new SortedList<int, string>();
-            var cat = article.Cat;
             var i = 0;
             while (cat != null)
             {
@@ -43,6 +43,24 @@ namespace BioEngine.Site.Components.Url
                 DbContext.Entry(article).Reference(x => x.Cat).Load();
             }
             var cat = article.Cat;
+            Poppulate(cat);
+
+            if (article.GameId > 0 && article.Game == null)
+            {
+                DbContext.Entry(article).Reference(x => x.Game).Load();
+            }
+            if (article.DeveloperId > 0 && article.Developer == null)
+            {
+                DbContext.Entry(article).Reference(x => x.Developer).Load();
+            }
+            if (article.TopicId > 0 && article.Topic == null)
+            {
+                DbContext.Entry(article).Reference(x => x.Topic).Load();
+            }
+        }
+
+        private void Poppulate(ArticleCat cat)
+        {
             while (cat != null)
             {
                 if (cat.ParentCat == null)
@@ -58,19 +76,33 @@ namespace BioEngine.Site.Components.Url
                 }
                 cat = cat.ParentCat;
             }
+        }
 
-            if (article.GameId > 0 && article.Game == null)
+        public string CatPublicUrl(ArticleCat cat, int page = 1)
+        {
+            Poppulate(cat);
+            var url = CatUrl(cat) + "/" + cat.Url;
+            if (page > 1)
             {
-                DbContext.Entry(article).Reference(x => x.Game).Load();
+                url += $"/page/{page}";
             }
-            if (article.DeveloperId > 0 && article.Developer == null)
-            {
-                DbContext.Entry(article).Reference(x => x.Developer).Load();
-            }
-            if (article.TopicId > 0 && article.Topic == null)
-            {
-                DbContext.Entry(article).Reference(x => x.Topic).Load();
-            }
+            return GetUrl("Show", "Articles",
+                new {parentUrl = ParentUrl(cat), url});
+        }
+
+        public string ParentArticlesUrl(Developer developer)
+        {
+            return UrlHelper.Action<ArticlesController>(x => x.ArticlesList(developer.Url));
+        }
+
+        public string ParentArticlesUrl(Game game)
+        {
+            return UrlHelper.Action<ArticlesController>(x => x.ArticlesList(game.Url));
+        }
+
+        public string ParentArticlesUrl(Topic topic)
+        {
+            return UrlHelper.Action<ArticlesController>(x => x.ArticlesList(topic.Url));
         }
     }
 }

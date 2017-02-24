cf477f79fe9818cba2d350962e5c74289e57fa8e
SonicGD <sonicgd@gmail.com>
2017-02-24 19:20:26 +0500

    #2 don't run search for empty query

--- src/Site/Controllers/SearchController.cs
+++ src/Site/Controllers/SearchController.cs
@@ -28,91 +28,95 @@ namespace BioEngine.Site.Controllers
         public async Task<IActionResult> Index(string query, string block)
         {
             var viewModel = new SearchViewModel(ViewModelConfig, query);
-
-            var hasBlock = !string.IsNullOrEmpty(block);
-            var limit = hasBlock ? 0 : 5;
-            if (!hasBlock || block == "games")
+            if (!string.IsNullOrEmpty(query))
             {
-                var games = SearchEntities<Game>(query, limit);
-                var gamesCount = CountEntities<Game>(query);
-                var searchBlock = CreateSearchBlock("Игры", UrlManager.Search.BlockUrl("games", query), gamesCount,
-                    games, x => x.Title,
-                    x => Task.FromResult(UrlManager.Games.PublicUrl(x)),
-                    x => x.NewsDesc);
-                viewModel.AddBlock(await searchBlock);
-            }
+                var hasBlock = !string.IsNullOrEmpty(block);
+                var limit = hasBlock ? 0 : 5;
+                if (!hasBlock || block == "games")
+                {
+                    var games = SearchEntities<Game>(query, limit);
+                    var gamesCount = CountEntities<Game>(query);
+                    var searchBlock = CreateSearchBlock("Игры", UrlManager.Search.BlockUrl("games", query), gamesCount,
+                        games, x => x.Title,
+                        x => Task.FromResult(UrlManager.Games.PublicUrl(x)),
+                        x => x.NewsDesc);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "news")
-            {
-                var news = SearchEntities<News>(query, limit);
-                var newsCount = CountEntities<News>(query);
-                var searchBlock = CreateSearchBlock("Новости", UrlManager.Search.BlockUrl("news", query), newsCount,
-                    news, x => x.Title,
-                    x => Task.FromResult(UrlManager.News.PublicUrl(x)),
-                    x => x.ShortText);
-                viewModel.AddBlock(await searchBlock);
-            }
+                if (!hasBlock || block == "news")
+                {
+                    var news = SearchEntities<News>(query, limit);
+                    var newsCount = CountEntities<News>(query);
+                    var searchBlock = CreateSearchBlock("Новости", UrlManager.Search.BlockUrl("news", query), newsCount,
+                        news, x => x.Title,
+                        x => Task.FromResult(UrlManager.News.PublicUrl(x)),
+                        x => x.ShortText);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "articles")
-            {
-                var articles = SearchEntities<Article>(query, limit);
-                var articlesCount = CountEntities<Article>(query);
-                var searchBlock = CreateSearchBlock("Статьи", UrlManager.Search.BlockUrl("articles", query),
-                    articlesCount,
-                    articles, x => x.Title,
-                    async x => await UrlManager.Articles.PublicUrl(x),
-                    x => x.Announce);
-                viewModel.AddBlock(await searchBlock);
-            }
+                if (!hasBlock || block == "articles")
+                {
+                    var articles = SearchEntities<Article>(query, limit);
+                    var articlesCount = CountEntities<Article>(query);
+                    var searchBlock = CreateSearchBlock("Статьи", UrlManager.Search.BlockUrl("articles", query),
+                        articlesCount,
+                        articles, x => x.Title,
+                        async x => await UrlManager.Articles.PublicUrl(x),
+                        x => x.Announce);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "articlesCats")
-            {
-                var articlesCats = SearchEntities<ArticleCat>(query, limit);
-                var articlesCatsCount = CountEntities<ArticleCat>(query);
-                var searchBlock = CreateSearchBlock("Категории статей", UrlManager.Search.BlockUrl("articlesCats", query),
-                    articlesCatsCount,
-                    articlesCats, x => x.Title,
-                    async x => await UrlManager.Articles.CatPublicUrl(x),
-                    x => x.Descr);
-                viewModel.AddBlock(await searchBlock);
-            }
+                if (!hasBlock || block == "articlesCats")
+                {
+                    var articlesCats = SearchEntities<ArticleCat>(query, limit);
+                    var articlesCatsCount = CountEntities<ArticleCat>(query);
+                    var searchBlock = CreateSearchBlock("Категории статей",
+                        UrlManager.Search.BlockUrl("articlesCats", query),
+                        articlesCatsCount,
+                        articlesCats, x => x.Title,
+                        async x => await UrlManager.Articles.CatPublicUrl(x),
+                        x => x.Descr);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "files")
-            {
-                var files = SearchEntities<File>(query, limit);
-                var filesCount = CountEntities<File>(query);
-                var searchBlock = CreateSearchBlock("Файлы", UrlManager.Search.BlockUrl("files", query),
-                    filesCount,
-                    files, x => x.Title,
-                    async x => await UrlManager.Files.PublicUrl(x),
-                    x => x.Announce);
-                viewModel.AddBlock(await searchBlock);
-            }
+                if (!hasBlock || block == "files")
+                {
+                    var files = SearchEntities<File>(query, limit);
+                    var filesCount = CountEntities<File>(query);
+                    var searchBlock = CreateSearchBlock("Файлы", UrlManager.Search.BlockUrl("files", query),
+                        filesCount,
+                        files, x => x.Title,
+                        async x => await UrlManager.Files.PublicUrl(x),
+                        x => x.Announce);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "filesCat")
-            {
-                var fileCats = SearchEntities<FileCat>(query, limit);
-                var fileCatsCount = CountEntities<FileCat>(query);
-                var searchBlock = CreateSearchBlock("Категории файлов", UrlManager.Search.BlockUrl("filesCat", query),
-                    fileCatsCount,
-                    fileCats, x => x.Title,
-                    async x => await UrlManager.Files.CatPublicUrl(x),
-                    x => x.Descr);
-                viewModel.AddBlock(await searchBlock);
-            }
+                if (!hasBlock || block == "filesCat")
+                {
+                    var fileCats = SearchEntities<FileCat>(query, limit);
+                    var fileCatsCount = CountEntities<FileCat>(query);
+                    var searchBlock = CreateSearchBlock("Категории файлов",
+                        UrlManager.Search.BlockUrl("filesCat", query),
+                        fileCatsCount,
+                        fileCats, x => x.Title,
+                        async x => await UrlManager.Files.CatPublicUrl(x),
+                        x => x.Descr);
+                    viewModel.AddBlock(await searchBlock);
+                }
 
-            if (!hasBlock || block == "galleryCats")
-            {
-                var galleryCats = SearchEntities<GalleryCat>(query, limit);
-                var galleryCatsCount = CountEntities<GalleryCat>(query);
-                var searchBlock = CreateSearchBlock("Категории картинок", UrlManager.Search.BlockUrl("galleryCats", query),
-                    galleryCatsCount,
-                    galleryCats, x => x.Title,
-                    async x => await UrlManager.Gallery.CatPublicUrl(x),
-                    x => x.Desc);
-                viewModel.AddBlock(await searchBlock);
+                if (!hasBlock || block == "galleryCats")
+                {
+                    var galleryCats = SearchEntities<GalleryCat>(query, limit);
+                    var galleryCatsCount = CountEntities<GalleryCat>(query);
+                    var searchBlock = CreateSearchBlock("Категории картинок",
+                        UrlManager.Search.BlockUrl("galleryCats", query),
+                        galleryCatsCount,
+                        galleryCats, x => x.Title,
+                        async x => await UrlManager.Gallery.CatPublicUrl(x),
+                        x => x.Desc);
+                    viewModel.AddBlock(await searchBlock);
+                }
             }
-
             return View(viewModel);
         }
 

dfde04338becd665dc2290b73d9cebfc445bb777
SonicGD <sonicgd@gmail.com>
2017-03-16 12:17:50 +0500

    #32 add Sitemap generation

---
+++ src/Site/Controllers/SitemapController.cs
@@ -0,0 +1,153 @@
+﻿using BioEngine.Common.Base;
+using BioEngine.Common.DB;
+using BioEngine.Site.Base;
+using BioEngine.Site.Components;
+using BioEngine.Site.Components.Url;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SimpleMvcSitemap;
+using SimpleMvcSitemap.News;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BioEngine.Site.Controllers
+{
+    public class SitemapController : BaseController
+    {
+        public SitemapController(BWContext context, ParentEntityProvider parentEntityProvider, UrlManager urlManager,
+           IOptions<AppSettings> appSettingsOptions)
+            : base(context, parentEntityProvider, urlManager, appSettingsOptions)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            List<SitemapIndexNode> sitemapIndexNodes = new List<SitemapIndexNode>
+            {
+                new SitemapIndexNode(Url.Action("Main","Sitemap")),
+                new SitemapIndexNode(Url.Action("News","Sitemap")),
+                new SitemapIndexNode(Url.Action("Games","Sitemap")),
+                new SitemapIndexNode(Url.Action("Articles","Sitemap")),
+                new SitemapIndexNode(Url.Action("Files","Sitemap")),
+                new SitemapIndexNode(Url.Action("Gallery","Sitemap")),
+            };
+
+            return new SitemapProvider().CreateSitemapIndex(new SitemapIndexModel(sitemapIndexNodes));
+        }
+
+        public ActionResult Main()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index", "News"))
+                {
+                    ChangeFrequency = ChangeFrequency.Daily,
+                    LastModificationDate = DateTime.UtcNow,
+                    Priority = 1
+                }
+            };
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+
+        public async Task<ActionResult> News()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+            var allNews = await Context.News.Where(x => x.Pub == 1)
+                    .OrderByDescending(x => x.Date)
+                    .Include(x => x.Author)
+                    .Include(x => x.Game)
+                    .Include(x => x.Developer)
+                    .Include(x => x.Topic).ToListAsync();
+            foreach (var news in allNews)
+            {
+                nodes.Add(new SitemapNode(UrlManager.News.PublicUrl(news, true))
+                {
+                    News = new SitemapNews(newsPublication: new NewsPublication(name: news.Title, language: "ru"),
+                           publicationDate: DateTimeOffset.FromUnixTimeSeconds(news.Date).Date,
+                           title: news.Title),
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    Priority = 0.9M
+                });
+            }
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+
+        public async Task<ActionResult> Games()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+            foreach (var game in await Context.Games.ToListAsync())
+            {
+                nodes.Add(new SitemapNode(UrlManager.Games.PublicUrl(game, true))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    LastModificationDate = DateTime.UtcNow,
+                    Priority = 0.9M
+                });
+            }
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+
+        public async Task<ActionResult> Articles()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+            foreach (var articleCat in await Context.ArticleCats.ToListAsync())
+            {
+                nodes.Add(new SitemapNode(await UrlManager.Articles.CatPublicUrl(articleCat))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    Priority = 0.9M
+                });
+            }
+            foreach (var article in await Context.Articles.Where(x => x.Pub == 1).ToListAsync())
+            {
+                nodes.Add(new SitemapNode(await UrlManager.Articles.PublicUrl(article))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    LastModificationDate = DateTimeOffset.FromUnixTimeSeconds(article.Date).Date,
+                    Priority = 0.9M
+                });
+            }
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+
+        public async Task<ActionResult> Files()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+            foreach (var fileCat in await Context.FileCats.ToListAsync())
+            {
+                nodes.Add(new SitemapNode(await UrlManager.Files.CatPublicUrl(fileCat))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    Priority = 0.9M
+                });
+            }
+            foreach (var file in await Context.Files.ToListAsync())
+            {
+                nodes.Add(new SitemapNode(await UrlManager.Files.PublicUrl(file))
+                {
+                    ChangeFrequency = ChangeFrequency.Monthly,
+                    LastModificationDate = DateTimeOffset.FromUnixTimeSeconds(file.Date).Date,
+                    Priority = 0.8M
+                });
+            }
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+
+        public async Task<ActionResult> Gallery()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>();
+            foreach (var galleryCat in await Context.GalleryCats.ToListAsync())
+            {
+                nodes.Add(new SitemapNode(await UrlManager.Gallery.CatPublicUrl(galleryCat))
+                {
+                    ChangeFrequency = ChangeFrequency.Weekly,
+                    Priority = 0.9M
+                });
+            }
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}

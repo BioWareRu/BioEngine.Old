c213f8488216d68a3e5999ae9ea3ae21333d397b
SonicGD <sonicgd@gmail.com>
2017-03-16 14:05:50 +0500

    #28 catch errors

--- src/Site/Filters/CounterFilter.cs
+++ src/Site/Filters/CounterFilter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using Prometheus;
+using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 
@@ -9,7 +11,14 @@ namespace BioEngine.Site.Filters
     {
         private static readonly ConcurrentDictionary<string, Summary> PathSummaries = new ConcurrentDictionary<string, Summary>();
 
+        private readonly ILogger _logger;
+
         public Stopwatch timer;
+
+        public CounterFilter(ILogger<CounterFilter> logger)
+        {
+            _logger = logger;
+        }
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
@@ -21,13 +30,25 @@ namespace BioEngine.Site.Filters
         {
             base.OnActionExecuted(context);
             timer.Stop();
-            GetSummary(context.ActionDescriptor.DisplayName).Observe(timer.ElapsedMilliseconds);
+            GetSummary(context.ActionDescriptor.DisplayName)?.Observe(timer.ElapsedMilliseconds);
         }
 
-        private static Summary GetSummary(string action)
+        private Summary GetSummary(string action)
         {
             return PathSummaries.GetOrAdd($"{action.Replace('.', '_')}",
-                newPath => Metrics.CreateSummary($"query_{newPath}_summary", $"Queries summary for {newPath}"));
+                newPath =>
+                {
+                    try
+                    {
+                        var metric = Metrics.CreateSummary($"query_{newPath}_summary", $"Queries summary for {newPath}");
+                        return metric;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError($"Can't create metric for {newPath}: {ex.Message}");
+                    }
+                    return null;
+                });
         }
     }
 }

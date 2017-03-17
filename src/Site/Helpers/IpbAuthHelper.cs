4ddc8830a403f82b4c2a2a5abb49f10b9896f3c4
SonicGD <sonicgd@gmail.com>
2017-03-17 14:40:22 +0500

    Refactor startup

---
+++ src/Site/Helpers/IpbAuthHelper.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace BioEngine.Site.Helpers
+{
+    public static class IpbAuthHelper
+    {
+        public static void ParseIpbResponse(string response, ClaimsIdentity identity, string claimsIssuer)
+        {
+            var user = JObject.Parse(response);
+
+            var identifier = user.Value<string>("id");
+            if (!string.IsNullOrEmpty(identifier))
+            {
+                identity.AddClaim(new Claim(
+                    ClaimTypes.NameIdentifier, identifier,
+                    ClaimValueTypes.String, claimsIssuer));
+            }
+
+            var userName = user.Value<string>("displayName");
+            if (!string.IsNullOrEmpty(userName))
+            {
+                identity.AddClaim(new Claim(
+                    ClaimsIdentity.DefaultNameClaimType, userName,
+                    ClaimValueTypes.String, claimsIssuer));
+            }
+
+            var profileUrl = user.Value<string>("profileUrl");
+            if (!string.IsNullOrEmpty(profileUrl))
+            {
+                identity.AddClaim(new Claim(
+                    ClaimTypes.Webpage, profileUrl,
+                    ClaimValueTypes.String, claimsIssuer));
+            }
+
+            var link = user.Value<string>("avatar");
+            if (!string.IsNullOrEmpty(link))
+            {
+                identity.AddClaim(new Claim(
+                    "avatarUrl", link,
+                    ClaimValueTypes.String, claimsIssuer));
+            }
+        }
+
+        public static void UseIpbOAuthAuthentication(this IApplicationBuilder app, IConfigurationRoot configuration)
+        {
+            app.UseOAuthAuthentication(new OAuthOptions()
+            {
+                SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme,
+                AuthenticationScheme = "IPB",
+                DisplayName = "IPB",
+                ClientId = configuration["IPB_OAUTH_CLIENT_ID"],
+                ClientSecret = configuration["IPB_OAUTH_CLIENT_SECRET"],
+                CallbackPath = new PathString(configuration["Data:OAuth:CallbackPath"]),
+                AuthorizationEndpoint = configuration["Data:OAuth:AuthorizationEndpoint"],
+                TokenEndpoint = configuration["Data:OAuth:TokenEndpoint"],
+                UserInformationEndpoint = configuration["Data:OAuth:UserInformationEndpoint"],
+                SaveTokens = true,
+                Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        // Get the IPB user
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        var response = await context.Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        ParseIpbResponse(await response.Content.ReadAsStringAsync(), context.Identity, context.Options.ClaimsIssuer);
+                    }
+                }
+            });
+        }
+    }
+}
--- src/Site/Startup.cs
+++ src/Site/Startup.cs
@@ -3,9 +3,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Security.Claims;
 using BioEngine.Common.Base;
 using BioEngine.Common.DB;
 using BioEngine.Common.Search;
@@ -15,7 +12,6 @@ using BioEngine.Site.Helpers;
 using cloudscribe.Syndication.Models.Rss;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -25,7 +21,6 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.HttpOverrides;
 using Serilog.Core;
 using Serilog;
@@ -76,24 +71,31 @@ namespace BioEngine.Site
                 .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization()
                 .AddTypedRouting();
+            services.AddDistributedMemoryCache();
+            services.AddResponseCaching();
             services.AddAuthentication(options => options.SignInScheme =
                 CookieAuthenticationDefaults.AuthenticationScheme);
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.CookieName = ".BioWareRu.Session";
+                options.CookieHttpOnly = true;
+            });
+
             services.Configure<AppSettings>(Configuration.GetSection("Application"));
+            services.AddDbContext<BWContext>();
+
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
             services.AddSingleton(Configuration);
             services.AddSingleton<DBConfiguration, MySqlDBConfiguration>();
 
-            services.AddDbContext<BWContext>();
             services.AddScoped<BannerProvider>();
             services.AddScoped<UrlManager>();
             services.AddScoped<ParentEntityProvider>();
             services.AddScoped<ContentHelper>();
             services.AddScoped<IChannelProvider, RssProvider>();
             services.AddScoped(typeof(ISearchProvider<>), typeof(ElasticSearchProvider<>));
-
-            services.AddDistributedMemoryCache();
-
-            services.AddResponseCaching();
 
             if (_env.IsProduction())
             {
@@ -112,14 +114,6 @@ namespace BioEngine.Site
                 services.AddDataProtection().PersistKeysToRedis(redis, "DataProtection-Keys");
                 services.AddAntiforgery(opts => opts.CookieName = "beAntiforgeryCookie");
             }
-
-            services.AddSession(options =>
-            {
-                // Set a short timeout for easy testing.
-                options.IdleTimeout = TimeSpan.FromMinutes(30);
-                options.CookieName = ".BioWareRu.Session";
-                options.CookieHttpOnly = true;
-            });
         }
 
         private static IPHostEntry TryResolveDns(string redisUrl)
@@ -128,13 +122,83 @@ namespace BioEngine.Site
             return ip;
         }
 
-
         public static readonly LoggingLevelSwitch LogLevelSwitch = new LoggingLevelSwitch(LogEventLevel.Warning);
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         [UsedImplicitly]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
             IApplicationLifetime lifetime)
+        {
+            ConfigureLogging(env, loggerFactory);
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("ru-RU"),
+                new CultureInfo("ru")
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ru-RU"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
+
+            if (env.IsProduction())
+            {
+                ConfigureProduction(app, lifetime);
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.UseMiddleware<CounterMiddleware>();
+
+            app.UseResponseCaching();
+
+            app.UseSession();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                LoginPath = new PathString("/login")
+            });
+
+            app.UseIpbOAuthAuthentication(Configuration);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Index}/{action=Index}/{id?}");
+            });
+        }
+
+        private void ConfigureProduction(IApplicationBuilder app, IApplicationLifetime lifetime)
+        {
+            var options = new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+            };
+            options.KnownProxies.Clear();
+            options.KnownNetworks.Clear();
+            options.RequireHeaderSymmetry = false;
+            app.UseForwardedHeaders(options);
+
+            app.UseExceptionHandler("/error/500");
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
+
+            lifetime.ApplicationStarted.Register(RunPrometheus);
+            lifetime.ApplicationStopped.Register(StopPrometheus);
+            lifetime.ApplicationStopped.Register(Log.CloseAndFlush);
+        }
+
+        private void ConfigureLogging(IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             var loggerConfiguration =
                 new LoggerConfiguration().MinimumLevel.ControlledBy(LogLevelSwitch).Enrich.FromLogContext();
@@ -158,129 +222,6 @@ namespace BioEngine.Site
             }
             Log.Logger = loggerConfiguration.CreateLogger();
             loggerFactory.AddSerilog();
-
-            var supportedCultures = new[]
-            {
-                new CultureInfo("ru-RU"),
-                new CultureInfo("ru")
-            };
-            
-            app.UseRequestLocalization(new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("ru-RU"),
-                // Formatting numbers, dates, etc.
-                SupportedCultures = supportedCultures,
-                // UI strings that we have localized.
-                SupportedUICultures = supportedCultures
-            });
-
-            if (env.IsProduction())
-            {
-                var options = new ForwardedHeadersOptions
-                {
-                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-                };
-                options.KnownProxies.Clear();
-                options.KnownNetworks.Clear();
-                options.RequireHeaderSymmetry = false;
-                app.UseForwardedHeaders(options);
-
-                app.UseExceptionHandler("/error/500");
-
-                app.UseStatusCodePagesWithReExecute("/error/{0}");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseMiddleware<LoggingMiddleware>();
-
-            app.UseMiddleware<CounterMiddleware>();
-
-            app.UseResponseCaching();
-
-            app.UseSession();
-
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = true,
-                LoginPath = new PathString("/login")
-            });
-
-            app.UseOAuthAuthentication(new OAuthOptions()
-            {
-                SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme,
-                AuthenticationScheme = "IPB",
-                DisplayName = "IPB",
-                ClientId = Configuration["IPB_OAUTH_CLIENT_ID"],
-                ClientSecret = Configuration["IPB_OAUTH_CLIENT_SECRET"],
-                CallbackPath = new PathString(Configuration["Data:OAuth:CallbackPath"]),
-                AuthorizationEndpoint = Configuration["Data:OAuth:AuthorizationEndpoint"],
-                TokenEndpoint = Configuration["Data:OAuth:TokenEndpoint"],
-                UserInformationEndpoint = Configuration["Data:OAuth:UserInformationEndpoint"],
-                SaveTokens = true,
-                Events = new OAuthEvents
-                {
-                    OnCreatingTicket = async context =>
-                    {
-                        // Get the GitHub user
-                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                        var response = await context.Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
-                        response.EnsureSuccessStatusCode();
-
-                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-                        var identifier = user.Value<string>("id");
-                        if (!string.IsNullOrEmpty(identifier))
-                        {
-                            context.Identity.AddClaim(new Claim(
-                                ClaimTypes.NameIdentifier, identifier,
-                                ClaimValueTypes.String, context.Options.ClaimsIssuer));
-                        }
-
-                        var userName = user.Value<string>("displayName");
-                        if (!string.IsNullOrEmpty(userName))
-                        {
-                            context.Identity.AddClaim(new Claim(
-                                ClaimsIdentity.DefaultNameClaimType, userName,
-                                ClaimValueTypes.String, context.Options.ClaimsIssuer));
-                        }
-
-                        var profileUrl = user.Value<string>("profileUrl");
-                        if (!string.IsNullOrEmpty(profileUrl))
-                        {
-                            context.Identity.AddClaim(new Claim(
-                                ClaimTypes.Webpage, profileUrl,
-                                ClaimValueTypes.String, context.Options.ClaimsIssuer));
-                        }
-
-                        var link = user.Value<string>("avatar");
-                        if (!string.IsNullOrEmpty(link))
-                        {
-                            context.Identity.AddClaim(new Claim(
-                                "avatarUrl", link,
-                                ClaimValueTypes.String, context.Options.ClaimsIssuer));
-                        }
-                    }
-                }
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Index}/{action=Index}/{id?}");
-            });
-
-            if (env.IsProduction())
-            {
-                lifetime.ApplicationStarted.Register(RunPrometheus);
-                lifetime.ApplicationStopped.Register(StopPrometheus);
-            }
-            lifetime.ApplicationStopped.Register(Log.CloseAndFlush);
         }
 
         private MetricServer _metricServer;

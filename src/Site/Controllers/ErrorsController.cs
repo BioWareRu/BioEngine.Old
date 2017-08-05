25ec6c86509f622df8bbb663e0c4e85465a0d0e5
SonicGD <sonicgd@gmail.com>
2017-08-05 19:17:02 +0500

    Log 404

--- src/Site/Controllers/ErrorsController.cs
+++ src/Site/Controllers/ErrorsController.cs
@@ -2,9 +2,11 @@
 using BioEngine.Common.DB;
 using BioEngine.Common.Interfaces;
 using BioEngine.Site.Base;
+using BioEngine.Site.Extensions;
 using BioEngine.Site.ViewModels.Errors;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace BioEngine.Site.Controllers
@@ -18,8 +20,12 @@ namespace BioEngine.Site.Controllers
         }
 
         [Route("/error/{errorCode}")]
-        public IActionResult Error(int errorCode)
+        public IActionResult Error(int errorCode, [FromServices] ILogger<ErrorsController> logger)
         {
+            if (errorCode == 404)
+            {
+                logger.LogError($"Page not found. Url: {HttpContext.Request.AbsoluteUrl()}");
+            }
             return View("Error", new ErrorsViewModel(ViewModelConfig, errorCode));
         }
     }

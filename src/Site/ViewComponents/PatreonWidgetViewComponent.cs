2ae9ac4d3e22a2af2785e5051dea8117a4ab7d3b
SonicGD <sonicgd@gmail.com>
2017-08-06 18:50:23 +0500

    #73 Don't crash on api fail)

--- src/Site/ViewComponents/PatreonWidgetViewComponent.cs
+++ src/Site/ViewComponents/PatreonWidgetViewComponent.cs
@@ -1,24 +1,37 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using BioEngine.Site.Helpers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BioEngine.Site.ViewComponents
 {
     public class PatreonWidgetViewComponent : ViewComponent
     {
         private readonly PatreonApiHelper _patreonApiHelper;
+        private readonly ILogger<PatreonApiHelper> _logger;
 
-        public PatreonWidgetViewComponent(PatreonApiHelper patreonApiHelper)
+        public PatreonWidgetViewComponent(PatreonApiHelper patreonApiHelper, ILogger<PatreonApiHelper> logger)
         {
             _patreonApiHelper = patreonApiHelper;
+            _logger = logger;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var goals = await _patreonApiHelper.GetGoals();
-            var currentGoal = goals.Where(x => x.CompletedPercentage < 100)
-                .OrderByDescending(x => x.CompletedPercentage).First();
+            PatreonGoal currentGoal;
+            try
+            {
+                var goals = await _patreonApiHelper.GetGoals();
+                currentGoal = goals.Where(x => x.CompletedPercentage < 100)
+                    .OrderByDescending(x => x.CompletedPercentage).First();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while loading patreon goals: {ex.Message}");
+                currentGoal = null;
+            }
             return View(currentGoal);
         }
     }

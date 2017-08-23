40bca944706c4b222f6537f141bd368e219cf664
SonicGD <sonicgd@gmail.com>
2017-08-24 00:38:32 +0500

    fix

--- src/API/Components/REST/Errors/ExceptionFilter.cs
+++ src/API/Components/REST/Errors/ExceptionFilter.cs
@@ -5,23 +5,19 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
+// ReSharper disable once RedundantUsingDirective
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BioEngine.API.Components.REST.Errors
 {
     [UsedImplicitly]
     public class ExceptionFilter : ExceptionFilterAttribute
     {
-        private readonly ILogger<ExceptionFilter> _logger;
-
-        public ExceptionFilter(ILogger<ExceptionFilter> logger)
-        {
-            _logger = logger;
-        }
-
         public override void OnException(ExceptionContext context)
         {
+            var logger = context.HttpContext.RequestServices.GetService<ILogger<ExceptionFilter>>();
             var exception = context.Exception;
-            _logger.LogError(500, exception, $"API Exception: {exception.Message}");
+            logger.LogError(500, exception, $"API Exception: {exception.Message}");
             context.Result =
                 new ObjectResult(new RestResult(StatusCodes.Status500InternalServerError,
                     new List<IErrorInterface> {new Models.RestError("Внутренняя ошибка сервера")}))

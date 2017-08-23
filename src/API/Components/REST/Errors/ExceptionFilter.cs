dfa20b29d454aea14b90df4b87751a1a6f586869
SonicGD <sonicgd@gmail.com>
2017-08-23 23:46:16 +0500

    Log exceptions

--- src/API/Components/REST/Errors/ExceptionFilter.cs
+++ src/API/Components/REST/Errors/ExceptionFilter.cs
@@ -4,14 +4,24 @@ using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace BioEngine.API.Components.REST.Errors
 {
     [UsedImplicitly]
     public class ExceptionFilter : ExceptionFilterAttribute
     {
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public override void OnException(ExceptionContext context)
         {
+            var exception = context.Exception;
+            _logger.LogError(500, exception, $"API Exception: {exception.Message}");
             context.Result =
                 new ObjectResult(new RestResult(StatusCodes.Status500InternalServerError,
                     new List<IErrorInterface> {new Models.RestError("Внутренняя ошибка сервера")}))

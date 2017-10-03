520e72b3592c5ba3fbaae05fa7911d0fcad945f1
Nadia <nadia__@bk.ru>
2017-10-03 11:49:47 +0500

    #82 update index

--- src/Data/Articles/Handlers/UpdateArticleCatHandler.cs
+++ src/Data/Articles/Handlers/UpdateArticleCatHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using BioEngine.Data.Articles.Commands;
 using BioEngine.Data.Core;
+using BioEngine.Data.Search.Commands;
 using FluentValidation;
 using JetBrains.Annotations;
 
@@ -18,6 +19,8 @@ namespace BioEngine.Data.Articles.Handlers
         protected override async Task<bool> ExecuteCommandAsync(UpdateArticleCatCommand command)
         {
             Mapper.Map(command, command.Model);
+            
+            await Mediator.Publish(new IndexEntityCommand<Common.Models.ArticleCat>(command.Model));
 
             DBContext.Update(command.Model);
             await DBContext.SaveChangesAsync();

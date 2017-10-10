4898069f6f8836c4f4764d53c38f2d5af6a86059
Nadia <nadia__@bk.ru>
2017-10-10 16:54:56 +0500

    null pid for articles categories

--- src/Data/Articles/Commands/CreateArticleCatCommand.cs
+++ src/Data/Articles/Commands/CreateArticleCatCommand.cs
@@ -24,7 +24,6 @@ namespace BioEngine.Data.Articles.Commands
         {
             RuleFor(x => x.Title).NotEmpty().MaximumLength(255);
             RuleFor(x => x.Url).NotEmpty().MaximumLength(255);
-            RuleFor(x => x.CatId).NotNull();
             RuleFor(x => x.GameId).SetValidator(new ChildValidator(true));
             RuleFor(x => x.DeveloperId).SetValidator(new ChildValidator(true));
             RuleFor(x => x.TopicId).SetValidator(new ChildValidator(true));
